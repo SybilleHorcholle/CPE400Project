12c3f84aa7ce57d8152e258e56496766b3320ad1
SybilleHorcholle <37204647+SybilleHorcholle@users.noreply.github.com>
2018-12-05 22:26:34 -0800

    Update dynamicRoutingProgram.py

--- dynamicRoutingProgram.py
+++ dynamicRoutingProgram.py
@@ -11,20 +11,15 @@ import random
 Class Name: node
 Parameters: self, id_num, deadEnd, active, visited, neighbors, failChance
 Functionality: This class creates a node for the mesh network. It has various member functions.
-    Member Functions: getID(self)
-                                        Returns the ID of the given node.
-                                    getStatus(self)
-                                        Returns the status of the given node, such as active.
-                                    setStatus(self, status)
-                                        This function will set the status of the node.
-                                    hasNeighbor()
-                                        Checks to see if a given node is a dead end node, meaning that it has only one neighbor.
-                                    checkNeighbor()
-                                        Checks to see if the node only has one neighbor, or less than one neighbor.  If it does, then it is a dead end node.
-                                        Otherwise, the dead end variable stays false, so it is still available.
-                                    fail()
-                                        Determines if a given node has failed.  It takes in a percent chance of failure, and generates a random number.
-                                        If that random number is less than or equal to the percent chance of failure, that means the node has failed.  
+    Member Functions: 
+        getID(self): Returns the ID of the given node.
+        getStatus(self): Returns the status of the given node, such as active.
+        setStatus(self, status): This function will set the status of the node.
+        hasNeighbor(): Checks to see if a given node is a dead end node, meaning that it has only one neighbor.
+        checkNeighbor(): Checks to see if the node only has one neighbor, or less than one neighbor.  If it does, then it is a dead end node.
+                         Otherwise, the dead end variable stays false, so it is still available.
+        fail(): Determines if a given node has failed.  It takes in a percent chance of failure, and generates a random number.
+                If that random number is less than or equal to the percent chance of failure, that means the node has failed.  
 Exceptions: NONE
 
 Important Notes: 
@@ -36,15 +31,14 @@ Important Notes:
     deadendFlag = boolean flag to determine if node has no further neighbors
     random.random() * 100
 """
+   
 class node:
-    def __init__(self, id_num, deadEnd=False, active=True, visited=False, neighbors={}, failChance=.5):
+    def __init__(self, id_num, deadEnd=False, active=True, neighbors={}, failChance=.5):
         self.ID = id_num
         self.deadEnd = deadEnd
         self.active = active
-        self.visited = visited
         self.neighborID = [neighbors]
         self.failChance = failChance
-        #package = 0
 
     def getID(self):
         return int(self.ID)
@@ -55,6 +49,9 @@ class node:
     def setStatus(self,status):
         self.active = status
     
+    def getFailChance(self):
+        return int(self.failChance)
+
     def hasNeighbor():
         if(node.deadEnd == True):
             print("no")
@@ -67,8 +64,6 @@ class node:
         listIndex = len(node.neighborID)
         if (listIndex <= 1):
             node.deadEnd = True
-            #for index in range(node.listIndex):
-                #print(node.neighborID[index])
 
     def fail():
         num = random.random() * 100
@@ -80,6 +75,7 @@ class node:
         else:
             print("Active")
 
+   
 """
 Function Name: minDistance
 Parameters: distances, path
@@ -97,7 +93,6 @@ def minDistance(distances, path):
             min = distances[i]
             min_index = i
     return min_index
-
 """
 Function Name: shortestPath
 Parameters: start_node, dest_node, edge_matrix
@@ -110,18 +105,25 @@ Important Notes:
     Function to find the shortest path between two given nodes. 
 """
 def shortestPath(start_node, dest_node, edge_matrix):
+    print("srcnode: ",start_node," destnode: ",dest_node)
+    visited = []
     distances = [np.inf] * num_nodes #holds the shortest distances from start_node to dest_node
     path = [False] * num_nodes  #holds nodes that are part of shortest path
     distances[start_node] = 0 # set distance from source node as 0
-    
-    for i in range(num_nodes):
+    current_node = start_node
+
+    while current_node != dest_node:
+        
         min_node = minDistance(distances, path)
+        current_node = min_node
         path[min_node] = True
         for j in range(num_nodes):
             if edge_matrix[min_node][j] > 0 and path[j] == False and distances[j] > distances[min_node] + edge_matrix[min_node][j]:
                 distances[j] = distances[min_node] + edge_matrix[min_node][j]
+                visited.append(j)
+    
+    print('visited: ',visited)
     return distances
-
 """
 Function Name: checkPaths
 Parameters: edge_matrix, node_list
@@ -136,9 +138,11 @@ def checkPaths(edge_matrix,node_list):
             if node_list[i].getStatus() == False: #check if node is not active
                 for j in range(len(edge_matrix[1])):
                     edge_matrix[i][j] = np.inf
+                    edge_matrix[j][i] = np.inf # remove distance for failed node in edge matrix
     return edge_matrix
+# ------------ MAIN PROGRAM -----------
 
-# ------------ MAIN PROGRAM ------------------------------------------------------------------------------------------------------------------------------------------------------------
+# matrix containing the distances to each node
 edge_matrix = [[0,4,np.inf,np.inf,np.inf,np.inf,np.inf],[4,0,8,np.inf,np.inf,11,np.inf],[np.inf,8,0,7,np.inf,np.inf,np.inf],[np.inf,np.inf,7,0,2,9,np.inf],[np.inf,np.inf,np.inf,2,0,np.inf,np.inf],[np.inf,11,np.inf,9,np.inf,0,6],[np.inf,np.inf,np.inf,np.inf,np.inf,6,0]]
 
 node_file = open('nodes.txt', 'r') #open network data file
@@ -155,7 +159,7 @@ while offset < len(lines):
         string = clean_line.split(':')
         new_node.append(string[1])
     
-    temp_node = node(new_node[0],False,True,False,new_node[1],new_node[2])
+    temp_node = node(new_node[0],False,True,new_node[1],new_node[2])
     node_list.append(temp_node)
     print("printing id: ",temp_node.getID())
     print(new_node)
@@ -164,11 +168,39 @@ while offset < len(lines):
 
 node_file.close()
 
+# Get input node from user
+check = False
+while check == False:
+    sourceNode = int(input('Enter the source node ID: '))
+    srcCheck = False
+    for i in range(len(node_list)):
+        if node_list[i].getID() == sourceNode:
+            srcCheck = True
+            check = True
+    if srcCheck == False:
+        print("Invalid source node ID \n")
+
+# Get dest node from user
+check = False
+while check == False:
+    destNode = int(input('Enter the destination node ID: '))
+    srcCheck = False
+    for i in range(len(node_list)):
+        if node_list[i].getID() == sourceNode:
+            srcCheck = True
+            check = True
+    if srcCheck == False:
+        print("Invalid destination node ID \n")
+
 num_nodes = len(node_list)
-#-------need to implement failed node algorithm-------
-node_list[0].setStatus(False)
 
-print(checkPaths(edge_matrix,node_list))
-#dijkstraPath(node_list[0].getID()-1,node_list[4].getID()-1,edge_matrix)
+# Randomly deactive nodes
+failNum = random.random() * 100
 
-# ------------ END of MAIN PROGRAM -------------------------------------------------------------------------------------------------------------------------------------------------
+updated_matrix = [[]]
+updated_matrix = checkPaths(edge_matrix,node_list)
+print(updated_matrix)
+
+print("distances: ",shortestPath(node_list[sourceNode-1].getID()-1,node_list[destNode-1].getID()-1,edge_matrix))
+
+# ------------ END OF MAIN PROGRAM -------------------------------------------------------------------------------------------------------------------------------------------------
